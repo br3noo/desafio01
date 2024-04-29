5c81971eefb7e0b392a602a10ad2ee2c10542229
br3noo <breno0.henrique5@gmail.com>
2024-04-29 11:09:03 -0300

    primeiro desafio

---
+++ index.js
@@ -0,0 +1,27 @@
+let nome = prompt("Qual será é o nome do herói?")
+let exp = prompt("Quanto XP o herói tem acumulado até agr?")
+
+if(exp <= 1000) {
+    console.log("O herói é nível ferro")
+}
+else if(exp >= 1001 && exp <= 2000){
+    console.log("O herói é nível bronze")
+}
+else if(exp >= 2001 && exp <= 5000){
+    console.log("O herói é nível prata")
+}
+else if(exp >= 5001 && exp <= 7000){
+    console.log("O herói é nível ouro")
+}
+else if(exp >= 7001 && exp <= 8000){
+    console.log("O herói é nível platina")
+}
+else if(exp >= 8001 && exp <=9000){
+    console.log("O herói é ascendente")
+}
+else if(exp >= 9001 && exp <= 10000){
+    console.log("O herói é imortal")
+}
+else{
+    console.log("O herói é radiante")
+}
